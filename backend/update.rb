77de45c03f9aa8dd2a7169601f669038d058368a
Michael Specht <micha.specht@gmail.com>
2010-09-02 16:23:26 +0200

    update: gzip files, remove temp yaml files

--- backend/update.rb
+++ backend/update.rb
@@ -8,3 +8,6 @@ FileUtils::mkpath('timely')
 
 system("ruby1.9.1 fetch.rb")
 system("ruby1.9.1 read.rb ./fetch/timely-#{timestamp}.yaml > ./timely/timely-#{timestamp}.txt")
+FileUtils::rm_f("./fetch/timely-#{timestamp}.yaml")
+system("gzip ./timely/timely-#{timestamp}.txt")
+
