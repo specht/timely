68a9092b30e520869faf461885def4d50e0cc747
Michael Specht <micha.specht@gmail.com>
2012-01-13 08:25:16 +0100

    some minor tweaks

--- node/express/public/javascripts/timely.js
+++ node/express/public/javascripts/timely.js
@@ -1,12 +1,12 @@
 now = 0.0;
-time_per_pixel = 0.09 * 100.0;
+time_per_pixel = 0.09;// * 100.0;
 var calendar = $.calendars.instance();
 var date = $.calendars.newDate();
 box_hash = {};
 
 function init()
 {
-    now = $.calendars.newDate().toJD() - 365 * 20 - 100;
+    now = $.calendars.newDate().toJD();
 }
 
 function update()
@@ -30,19 +30,30 @@ function update()
                     date = calendar.formatDate("M d YYYY", calendar.fromJD(element.t));
                     text = date + ' &ndash; ' + text;
                     if (element.type == 'd')
-                        text = "<span style='color: #cc0000; font-weight: bold;'>&dagger;</span>&nbsp" + text;
+                        text = "<span style='color: #cc0000; font-weight: bold;'>&dagger;</span>&nbsp;" + text;
                     else if (element.type == 'b')
-                        text = "<span style='color: #4e9a06; font-weight: bold;'>&lowast;</span>&nbsp" + text;
+                        text = "<span style='color: #4e9a06; font-weight: bold;'>&lowast;</span>&nbsp;" + text;
                     $('div#canvas').append("<div class='box' style='z-index: " + element.relevance + ";' id='e" + element.id + "'>" + text + "</div>");
                     box = $('#e' + element.id);
                     box_hash[element.id] = box;
+                    box.data('relevance', element.relevance);
+                    box.mouseenter(function() {
+                        $(this).css('z-index', '999999999');
+                    });
+                    box.mouseleave(function() {
+                        $(this).css('z-index', $(this).data('relevance'));
+                    });
                     box.css('left', '' + Math.random() * ($('#canvas').width() - 300) + 'px');
+                    box.css('opacity', 0.0);
+                    box.css('top', '' + Math.floor((element.t - time_min) / (time_max - time_min) * canvas_height - box.height() / 2) + 'px');
+                    box.animate({'opacity': 1.0});
                 }
                 else
                 {
                     box = box_hash[element.id];
+                    box.animate({'top': '' + Math.floor((element.t - time_min) / (time_max - time_min) * canvas_height - box.height() / 2) + 'px'});
                 }
-                box.css('top', '' + Math.floor((element.t - time_min) / (time_max - time_min) * canvas_height - box.height() / 2) + 'px');
+//                 box.css('top', '' + Math.floor((element.t - time_min) / (time_max - time_min) * canvas_height - box.height() / 2) + 'px');
             });
             console.log("Got " + Object.keys(box_hash).length + " boxes on the canvas.");
         }
@@ -67,7 +78,7 @@ $(document).mousewheel(function(event, delta) {
             time_per_pixel *= 0.92;
     }
     else
-        now -= delta * time_per_pixel * 16;
+        now -= delta * time_per_pixel * 100;
     update();
 });
     
